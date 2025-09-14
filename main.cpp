3a492914c42ee879be9cae190508756e2a0a3b56
aautin <alexandreautin.aa@gmail.com>
2025-09-14 19:24:52 +0200

    link close event with confirmation pop-up to button

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QMessageBox>
+
+#include <QObject>
+
+void onButtonClicked(QApplication *app) {
+    qInfo("Button was clicked!");
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(nullptr, "Info", "Button clicked, application will close.",
+                                    QMessageBox::Yes | QMessageBox::No);
+    if (reply == QMessageBox::Yes)
+        app->quit();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QPushButton button("Hello Qt!");
+    QPushButton button("Close Application");
     button.resize(200, 100);
+    QObject::connect(&button, &QPushButton::clicked, [&app]() { onButtonClicked(&app); });
     button.show();
 
     return app.exec();
